95eb9e769e1ce2964f0c549c61238bfa94f46abe
tyadav <tushar.yadav@sowermate.com>
2024-09-16 12:27:23 +0530

    Remove the unwanted code

--- lib/services/employee_service.dart
+++ lib/services/employee_service.dart
@@ -1,25 +1,30 @@
+import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
-
+import 'package:http/http.dart' as http;
 import 'package:employee_management_system/model/employee_model.dart';
 import 'package:get/get.dart';
 
 class EmployeeService extends GetConnect implements GetxService {
   Future<List<EmployeeModel>> fetchEmployees() async {
     try {
-      Response response = await get(
-          'https://669b3f09276e45187d34eb4e.mockapi.io/api/v1/employee');
+      final response = await http.get(
+        Uri.parse(
+            'https://669b3f09276e45187d34eb4e.mockapi.io/api/v1/employee'),
+      );
+
       log(response.body.toString());
-      if (response.status.hasError) {
-        log(response.body.toString());
-        throw response.body.toString();
+
+      if (response.statusCode == 200) {
+        // Parse the JSON response
+        final List<dynamic> jResponse = jsonDecode(response.body);
+        return jResponse.map((e) => EmployeeModel.fromJson(e)).toList();
       } else {
-        final jResponse = response.body as List<dynamic>;
-        final data = jResponse.map((e) => EmployeeModel.fromJson(e)).toList();
-        log(data.toString());
-        return data;
+        // Handle the error response
+        log('Error: ${response.statusCode}');
+        throw Exception('Failed to load employees');
       }
     } catch (e) {
+      log('Exception: $e');
       rethrow;
     }
   }
